2253cbecbc4114dbbdb4f68827bb1b563aa1b789
Christopher B. Martin <magnellicoctaroon@hotmail.com>
2019-02-18 18:44:42 -0600

    I think this adds a button that'll save everything. I'll have to test it out later

--- WpfApp1/UI/Assignment/AssignmentPage.xaml.cs
+++ WpfApp1/UI/Assignment/AssignmentPage.xaml.cs
@@ -1,14 +1,19 @@
 ﻿using ClassroomAssignment.Model;
+using ClassroomAssignment.Repo;
 using ClassroomAssignment.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -43,6 +48,50 @@ namespace ClassroomAssignment.UI.Assignment
             var window = Window.GetWindow(this);
             if (window == null) return;
             window.SizeToContent = SizeToContent.Width;
+        }
+
+        void onClickSave(object sender, RoutedEventArgs e)
+        {
+            save();
+        }
+
+        void save()
+        {
+            SaveFileDialog saveFileDialog2 = new SaveFileDialog();
+            saveFileDialog2.Filter = "Assignment File | *.agn";
+
+            if (saveFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = saveFileDialog2.FileName;
+
+                try
+                {
+                    List<Course> originalCourses = GetOriginalCourses();
+                    AppState appState = new AppState(originalCourses, GetUpToDateCourses());
+
+                    IFormatter formatter = new BinaryFormatter();
+                    Stream stream = File.Open(fileName, FileMode.Create, FileAccess.Write);
+
+                    formatter.Serialize(stream, appState);
+                    stream.Close();
+
+                }
+                catch (SerializationException a)
+                {
+                    Console.WriteLine("Failed to deserialize. Reason: " + a.Message);
+                }
+
+            }
+        }
+
+        private List<Course> GetOriginalCourses()
+        {
+            return App.Current.Resources["originalCourses"] as List<Course>;
+        }
+
+        private List<Course> GetUpToDateCourses()
+        {
+            return CourseRepository.GetInstance().Courses.OrderBy(x => int.Parse(x.ClassID)).ToList();
         }
 
     }
